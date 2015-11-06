9ee7ddf19e217f120d27191a70bd1360df323594
cesine <cesine@yahoo.com>
2015-11-06 00:13:25 -0500

    added a form to request path from one node to another

--- src/app/components/view-path/view-path.controller.js
+++ src/app/components/view-path/view-path.controller.js
@@ -9,6 +9,9 @@
         node: '=model'
       },
       controller: function($scope) {
+        $scope.fromId = '123abc';
+        $scope.toId = '321swf';
+        
         $scope.path = {
           from: {
             id: '123abc',
@@ -33,8 +36,48 @@
             full_name: 'Valua',
             thumbnail_url: 'http://thumb9.shutterstock.com/thumb_large/93178/93178,1226420551,2/stock-photo-close-up-portrait-of-elegance-teenage-girl-isolated-on-white-background-20364880.jpg'
           }]
-        }
+        };
 
+        $scope.nodes = [{
+          id: 'k3ewsd',
+          full_name: 'Peter',
+          thumbnail_url: 'http://thumb7.shutterstock.com/display_pic_with_logo/384199/197274905/stock-photo-film-toned-grainy-portrait-of-laughing-young-employee-197274905.jpg'
+        }, {
+          id: '89j3w',
+          full_name: 'Joan',
+          thumbnail_url: 'http://thumb7.shutterstock.com/display_pic_with_logo/463936/96418745/stock-photo-asian-woman-smiling-happy-portrait-beautiful-mature-middle-aged-chinese-asian-woman-closeup-beauty-96418745.jpg'
+        }, {
+          id: 'oai32',
+          full_name: 'Valua',
+          thumbnail_url: 'http://thumb9.shutterstock.com/thumb_large/93178/93178,1226420551,2/stock-photo-close-up-portrait-of-elegance-teenage-girl-isolated-on-white-background-20364880.jpg'
+        }];
+
+        $scope.findPath = function(fromId, toId) {
+          console.log('requesting path from ' + fromId + ' to ' + toId);
+          $scope.status = "loading...";
+          CORS
+            .makeCORSRequest({
+              url: localStorage.getItem('neo4j_url'),
+              query: ''
+            })
+            .then(function(response) {
+              console.log('Received response', response);
+              $scope.path = response;
+            }, function(error) {
+              console.log('There was a bug', error);
+              $scope.error = error.userFriendlyErrorMessages.join(',');
+            })
+            .fail(function(exception) {
+              console.log('There was a bug', exception);
+              $scope.error = exception.message;
+            }).done(function() {
+              $scope.status = "";
+              if (!$scope.$$phase) {
+                $scope.$digest();
+              }
+
+            });
+        };
       }
     };
   });
